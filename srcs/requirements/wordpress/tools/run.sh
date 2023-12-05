3ba9cb0aeb1a537adc1001acfc22446d615446b5
rachidamhouch <rachid.amhouch@gmail.com>
2023-12-05 15:12:12 +0100

    Mandatory done

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -2,7 +2,7 @@
 cd /var/www/html/wordpress
 if [ ! -e wp-config.php ]; then
     wp --allow-root config create --dbhost=mariadb --dbname=$DB_NAME --dbuser=$USERNAME --dbpass=$USER_PASS
-    wp --allow-root core install --url=https://ramhouch.1337.ma/ --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_PASS --admin_email=$WORDPRESS_EMAIL
+    wp --allow-root core install --url=https://ramhouch.42.fr/ --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN" --admin_password="$WORDPRESS_PASS" --admin_email="$WORDPRESS_EMAIL"
 fi
 if [ -e wp-config.php ]; then
     php-fpm8.2 -F
