8b76241e31fb099c590833af9327fb026ac7c0e6
rachidamhouch <rachid.amhouch@gmail.com>
2023-12-06 12:28:06 +0100

    redis

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -5,5 +5,6 @@ if [ ! -e wp-config.php ]; then
     wp --allow-root core install --url=https://ramhouch.42.fr/ --title="$WORDPRESS_TITLE" --admin_user="$WORDPRESS_ADMIN" --admin_password="$WORDPRESS_PASS" --admin_email="$WORDPRESS_EMAIL"
 fi
 if [ -e wp-config.php ]; then
+    echo "define('WP_REDIS_CLIENT', 'predis');define('WP_REDIS_HOST', 'redis');define('WP_REDIS_PORT', 6379);" >> wp-config.php
     php-fpm8.2 -F
 fi
