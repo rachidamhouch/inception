a1088eb71a80638752ed053400a913c03ae81483
rachidamhouch <rachid.amhouch@gmail.com>
2023-12-06 12:40:18 +0100

    ftp fixed

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 service vsftpd start
-
 useradd -m $FTPUSER
 echo  $FTPUSER:$FTPPASS | /usr/sbin/chpasswd
-chown $FTPUSER:$FTPUSER -R /home/$FTPUSER/
+chown $FTPUSER:$FTPUSER -R /var/www/html/wordpress
 echo  $FTPUSER > /etc/vsftpd.userlist 
 service vsftpd stop
-
 vsftpd
