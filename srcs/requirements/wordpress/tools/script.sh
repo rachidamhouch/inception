f3a9a12fe9c17bcd1ae2ed1b7f174d5f13936351
rachidamhouch <rachid.amhouch@gmail.com>
2023-12-06 13:18:18 +0100

    Done

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -7,6 +7,5 @@ cd /var/www/html/wordpress
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-
 wp --allow-root core download
 sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
