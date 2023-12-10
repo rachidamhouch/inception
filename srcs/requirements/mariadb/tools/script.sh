b5fb6be3808f1993376e853ab3eda5cdd5ca5792
rachidamhouch <rachid.amhouch@gmail.com>
2023-12-10 19:52:02 +0100

    Finish

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -5,8 +5,7 @@ service mariadb start
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
 echo "CREATE USER '$USERNAME'@'%' IDENTIFIED BY '$USER_PASS';" >> db1.sql
 echo "USE rachid; GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USERNAME'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 mysql < db1.sql
 service mariadb stop
-mysqld -u root
+mysqld
